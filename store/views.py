e34d5a5a44922cfeae542a72fced8b2ef29bdd5f
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-05-29 15:29:58 -0700

    added sorting, filtering and pagination

--- store/views.py
+++ store/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+#* sorting, filtering and pagination
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+#* end
 # Create your views here.
 from .models import Book, Category, Review
 from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
@@ -10,6 +16,11 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend ,SearchFilter, OrderingFilter]
+    pagination_class = PageNumberPagination
+    search_fields = ['title__istartswith','author']
+    ordering_fields = ['length', 'created_at']
+    filterset_fields = ['title', 'author']
 
 
 class CategoryViewSet(ModelViewSet):
