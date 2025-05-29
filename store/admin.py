b186a3a8618cd37f5f6e3e0b92649bb1a7706d5d
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-05-29 15:50:29 -0700

    admin stuff added

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,42 @@
+from itertools import count
+
 from django.contrib import admin
+from social_core.pipeline import user
 
 from .models import Book, Category, Review
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Category)
-admin.site.register(Review)
+
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author','price', 'length','category', 'created_at', 'length_message']
+    search_fields = ['title', 'author']
+    list_editable = ['price']
+    autocomplete_fields = ['category']
+    list_filter = ['category','created_at', 'updated_at']
+    list_per_page = 10
+
+
+    @admin.display(ordering='length')
+    def length_message(self, obj):
+        if obj.length < 100:
+            return 'short read'
+        elif 100 < obj.length < 500:
+            return 'decent length'
+        else:
+            return 'long read'
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['book','description','user','created_at','updated_at']
+
+
+
+
+
